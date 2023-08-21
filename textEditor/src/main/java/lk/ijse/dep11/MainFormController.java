ba13653d55d600f6b2ef08cc561643578b8d4c1b
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-21 20:50:39 +0530

    Commit-3

--- textEditor/src/main/java/lk/ijse/dep11/MainFormController.java
+++ textEditor/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,33 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public AnchorPane root;
+    public MenuItem itmFileNew;
+    public MenuItem itmFileOpen;
+    public MenuItem itmFileSave;
+    public MenuItem itmFileSaveAs;
+    public MenuItem itmFilePrint;
+    public MenuItem itmFileExit;
+    public MenuItem itmEditCut;
+    public MenuItem itmEditCopy;
+    public MenuItem itmEditPaste;
+    public MenuItem itmEditSelectAll;
+    public MenuItem itmHelpUserGuide;
+    public MenuItem itmHelpAbout;
+
+    public void itmFileNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itmFileExitOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itmHelpUserGuideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itmHelpAboutOnAction(ActionEvent actionEvent) {
+    }
 }
