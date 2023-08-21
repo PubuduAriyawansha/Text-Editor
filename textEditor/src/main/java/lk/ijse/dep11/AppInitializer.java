4711317df7a083bc29727fc9f8df0e19235bf019
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-21 20:39:35 +0530

    Commit-2

--- textEditor/src/main/java/lk/ijse/dep11/AppInitializer.java
+++ textEditor/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -18,9 +21,21 @@ public class AppInitializer extends Application {
     public void start(Stage primaryStage) throws IOException {
         AnchorPane root = FXMLLoader.load(getClass().getResource("/view/SplashForm.fxml"));
         Scene Splash = new Scene(root);
-        primaryStage.setScene(Splash);
-        primaryStage.setTitle("Text Editor");
-        primaryStage.centerOnScreen();
-        primaryStage.show();
+     //   primaryStage.setScene(Splash);
+       // primaryStage.setTitle("Text Editor");
+
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+//        root.setBackground(Background.fill(Color.TRANSPARENT));
+//        Splash.setFill(Color.TRANSPARENT);
+
+//        primaryStage.centerOnScreen();
+//        primaryStage.show();
+        Splash.setFill(Color.TRANSPARENT);
+
+        Stage stage = new Stage(StageStyle.TRANSPARENT);
+        stage.setTitle("Text Editor");
+        stage.setScene(Splash);
+        stage.show();
+        stage.centerOnScreen();
     }
 }
