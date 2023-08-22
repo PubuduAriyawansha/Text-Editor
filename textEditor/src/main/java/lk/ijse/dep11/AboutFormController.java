26b9c40086cd48b5ee57e5953bcf6a6497a42ecf
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-22 09:34:07 +0530

    Commit-6

--- textEditor/src/main/java/lk/ijse/dep11/AboutFormController.java
+++ textEditor/src/main/java/lk/ijse/dep11/AboutFormController.java
@@ -1,4 +1,35 @@
 package lk.ijse.dep11;
 
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
 public class AboutFormController {
+    public AnchorPane root;
+    private double Xcordinate=0;
+    private double Ycordinate=0;
+
+    public void initialize(){
+        root.setOnMousePressed(this::rootMousePressed);
+        root.setOnMouseDragged(this::rootMouseDraggedAction);
+    }
+
+//    public void rootMouseClicked(MouseEvent mouseEvent) {
+//        Xcordinate=mouseEvent.getX();
+//        Ycordinate=mouseEvent.getY();
+//
+//    }
+
+    public void rootMouseDraggedAction(MouseEvent mouseEvent) {
+        Stage stage = (Stage)root.getScene().getWindow();
+        stage.setX(mouseEvent.getScreenX()-Xcordinate);
+        stage.setY(mouseEvent.getScreenY()-Ycordinate);
+
+    }
+
+    public void rootMousePressed(MouseEvent mouseEvent) {
+        Xcordinate=mouseEvent.getX();
+        Ycordinate=mouseEvent.getY();
+    }
 }
